5e702882ca2913d0b8d4bcc82f37e6bdd5fef710
SV <SV@PM19-PC13>
2024-09-17 14:55:56 +0700

    Add project files.

---
+++ 23DH110315_Mau9/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _23DH110315_Mau9.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult ProductDetail()
+        {
+            return View();
+        }
+        public ActionResult ShoppingCart()
+        {
+            return View();
+        }
+    }
+}
